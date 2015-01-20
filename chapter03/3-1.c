70c8c2ffebfb3da5b2145c96f4f43fc4b08f65c9
hfutwyy <hfutwyy@qq.com>
2015-01-20 07:43:31 -0800

    chapter 03 demos

---
+++ chapter03/3-1.c
@@ -0,0 +1,12 @@
+#include "apue.h"
+#include "myerr.h"
+
+int main(void)
+{
+	if(lseek(STDIN_FILENO, 0, SEEK_CUR) == -1) {
+	    printf("Can not seek\n");
+	} else {
+	    printf("seek Ok\n");
+	}
+	exit(0);
+}
