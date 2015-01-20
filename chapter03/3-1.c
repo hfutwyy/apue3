1984dccbe4e3590733a85f7e7287870cfb722dba
hfutwyy <hfutwyy@qq.com>
2015-01-20 07:48:33 -0800

    format code

--- chapter03/3-1.c
+++ chapter03/3-1.c
@@ -1,11 +1,15 @@
+//lseek demo
 #include "apue.h"
 #include "myerr.h"
 
 int main(void)
 {
-	if(lseek(STDIN_FILENO, 0, SEEK_CUR) == -1) {
+	if (lseek(STDIN_FILENO, 0, SEEK_CUR) == -1)
+        {
 	    printf("Can not seek\n");
-	} else {
+	}
+        else
+        {
 	    printf("seek Ok\n");
 	}
 	exit(0);
